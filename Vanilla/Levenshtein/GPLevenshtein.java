c03f27575be99c841543d7e1425cd046a39f02c9
Noémien Kocher <nkcr.je@gmail.com>
2016-04-06 09:39:18 +0200

    code clean, graph update

--- Vanilla/Levenshtein/GPLevenshtein.java
+++ Vanilla/Levenshtein/GPLevenshtein.java
@@ -1,49 +1,50 @@
 public class GPLevenshtein {
 
-    public static int distance(String a, String b) {
-        a = a.toLowerCase();
-        b = b.toLowerCase();
-        // i == 0
-        int [] costs = new int [b.length() + 1];
-        for (int j = 0; j < costs.length; j++)
-            costs[j] = j;
-        for (int i = 1; i <= a.length(); i++) {
-            // j == 0; nw = lev(i - 1, j)
-            costs[0] = i;
-            computeRow(costs, i, a, b);
-        }
-        return costs[b.length()];
+  public static void main(String [] args) {
+
+    if(args.length < 2) {
+      System.err.println("Needs two arguments: <string a> <string b>");
+      System.exit(0);
     }
+    String a = args[0];
+    String b = args[1];
 
-    public static void main(String [] args) {
+    // Benchmark initialization
+    long startTime = System.currentTimeMillis();
+    int dist = distance(a, b);
+    long stopTime = System.currentTimeMillis();
 
-      if(args.length < 2) {
-        System.err.println("Needs two arguments: <string a> <string b>");
-        System.exit(0);
-      }
-      String a = args[0];
-      String b = args[1];
+    // Compute elapsed time
+    long elapsedTime = stopTime - startTime;
+    System.out.println(elapsedTime/1000);
 
-      // Benchmark initialization
-      long startTime = System.currentTimeMillis();
-      int dist = distance(a, b);
-      long stopTime = System.currentTimeMillis();
+    // System.out.println("distance: " + dist);
+  }
 
-      // Compute elapsed time
-      long elapsedTime = stopTime - startTime;
-      System.out.println(elapsedTime/1000);
-
-
-      // System.out.println("distance: " + dist);
+  // Compute the distance between a and b
+  public static int distance(String a, String b) {
+    a = a.toLowerCase();
+    b = b.toLowerCase();
+    // i == 0
+    int [] costs = new int [b.length() + 1];
+    for (int j = 0; j < costs.length; j++)
+      costs[j] = j;
+    for (int i = 1; i <= a.length(); i++) {
+      // j == 0; nw = lev(i - 1, j)
+      costs[0] = i;
+      computeRow(costs, i, a, b);
     }
+    return costs[b.length()];
+  }
 
-    public static void computeRow(int[] costs, int i, String a, String b) {
-      int nw = i - 1;
-      for (int j = 1; j <= b.length(); j++) {
-          int cj = Math.min(1 + Math.min(costs[j], costs[j - 1]), a.charAt(i - 1) == b.charAt(j - 1) ? nw : nw + 1);
-          nw = costs[j];
-          costs[j] = cj;
-      }
+  // Compute a single row in the distance process
+  public static void computeRow(int[] costs, int i, String a, String b) {
+    int nw = i - 1;
+    for (int j = 1; j <= b.length(); j++) {
+      int cj = Math.min(1 + Math.min(costs[j], costs[j - 1]), a.charAt(i - 1) == b.charAt(j - 1) ? nw : nw + 1);
+      nw = costs[j];
+      costs[j] = cj;
     }
+  }
 
 }
--- Vanilla/Levenshtein/stats/gnuplot.sh
+++ Vanilla/Levenshtein/stats/gnuplot.sh
@@ -5,6 +5,8 @@ set title "GPLevenshtein - time over strings size"
 set xlabel "Strings size"
 set ylabel "Time [s]"
 
+set label "2.53 GHz Intel Core 2 Duo" at screen 0.5,0.5 center
+
 set samples 100000
 
 plot [0:90000] [0:63] 0.00000001*(x-10000)**2, 'GPLevenshtein-1-30000-100.csv' with lines
