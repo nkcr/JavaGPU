83a3289b8f1a864907b782eecd6ce7e5ff7b91dc
Noémien Kocher <nkcr.je@gmail.com>
2016-04-30 13:38:35 +0200

    plot color

--- Results/time_aparapi_leven_gnuplot.sh
+++ Results/time_aparapi_leven_gnuplot.sh
@@ -8,5 +8,5 @@ set ylabel "Time [s]"
 set label "Intel(R) Xeon(R) CPU E5-2609 v2 @ 2.50GHz - versus" at screen 0.5,0.5 center
 set label "Nvidia Tesla C2075 - double precision" at screen 0.5,0.4 center
 
-plot '../Analysis/Aparapi/Levenshtein/stats/10k-50k-100k-200k-300k-400k-500k-x5.csv' with lines lt 7,\
+plot '../Analysis/Aparapi/Levenshtein/stats/10k-50k-100k-200k-300k-400k-500k-x5.csv' with lines lt 4,\
      '../Vanilla/Levenshtein/stats/10k-50k-100k-200k-300k-400k-500k.csv' with lines lt 1
--- Results/time_aparapi_matrix_gnuplot.sh
+++ Results/time_aparapi_matrix_gnuplot.sh
@@ -8,5 +8,5 @@ set ylabel "Time [s]"
 set label "Intel(R) Xeon(R) CPU E5-2609 v2 @ 2.50GHz - versus" at screen 0.5,0.5 center
 set label "Nvidia Tesla C2075 - double precision" at screen 0.5,0.4 center
 
-plot '../Analysis/Aparapi/Matrix/stats/GPMatrix-100-3000-100-x5.csv' with lines lt 7,\
+plot '../Analysis/Aparapi/Matrix/stats/GPMatrix-100-3000-100-x5.csv' with lines lt 4,\
      '../Vanilla/Matrix/stats/GPMatrix-100-3000-100.csv' with lines lt 1
